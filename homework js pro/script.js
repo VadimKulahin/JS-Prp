29c7a5ba3ddb0b9506facda29e938bb4ca6039e7
Вадим Кулагин <125271432+VadimKulahin@users.noreply.github.com>
2023-05-31 14:04:52 +0200

    Add files via upload

---
+++ homework js pro/script.js
@@ -0,0 +1,21 @@
+function alphabetOrder(str) {
+    return str.split('').sort().join('');
+  }
+  
+  document.writeln(alphabetOrder("uncopyrightable"));
+
+
+  function findLongestWord(str) {
+    let words = str.split(' ');
+    let longestWord = '';
+    
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > longestWord.length) {
+        longestWord = words[i];
+      }
+    }
+    
+    return longestWord;
+  }
+  
+  document.writeln(findLongestWord('Web Development In Telran'));
